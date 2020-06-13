b56bb451ca6273e0dff82b233e730130e11d643b
Hari <simplyjavascript@gmail.com>
2020-06-13 13:39:23 -0400

    class code till date

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import Logo from "./components/Logo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   return (
+//     <div className="header_wrapper appRoot">
+//       <Logo />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { Component } from "react";
+import Logo from "./components/Logo";
+export default class App extends Component {
+  state = {
+    iconClass: "fa fa-film fa-3x",
+    logoText: "Black & White Frames",
+    inputVal: "Testing",
+  };
+
+  handleClick = () => {
+    console.log("clicked the button");
+    this.setState(() => {
+      return {
+        iconClass: "fa fa-leaf fa-3x",
+        logoText: "Save our planet",
+      };
+    });
+  };
+  handleChange = (e) => {
+    const val = e.target.value;
+    this.setState(
+      (state, props) => {
+        return {
+          inputVal: val,
+        };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Logo data={this.state} />
+        <h1> {this.state.inputVal}</h1>
+        <input
+          value={this.state.inputVal}
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button onClick={this.handleClick} className="btn">
+          Change
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = (props) => {
+  const { iconClass, logoText } = props.data;
+  return (
+    <div>
+      <div className="header_logo">
+        <i className={iconClass}></i>
+        <span className="logo_text"> {logoText} </span>
+      </div>
+      <div className="divider div-transparent div-dot"></div>
+    </div>
+  );
+};
+
+export default Logo;
